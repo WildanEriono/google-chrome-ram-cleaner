c5daabdf4fca5d63204f554893215b2941df4f53
Wildan Eriono <149647285+WildanEriono@users.noreply.github.com>
2023-11-18 14:14:09 +0700

    Add files via upload

---
+++ Memory Cleaning Chrome/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Runtime.InteropServices
+Imports Microsoft.VisualBasic.CompilerServices
+
+Public Class Form1
+    <DllImport("kernel32.dll", CharSet:=CharSet.Ansi, SetLastError:=True, ExactSpelling:=True)>
+    Public Shared Function SetProcessWorkingSetSize(ByVal process As IntPtr, ByVal minimumWorkingSetSize As Integer, ByVal maximumWorkingSetSize As Integer) As Integer
+    End Function
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            GC.Collect()
+            GC.WaitForPendingFinalizers()
+
+            If (Environment.OSVersion.Platform = PlatformID.Win32NT) Then
+                Form1.SetProcessWorkingSetSize(Process.GetCurrentProcess.Handle, -1, -1)
+                Dim processesByName As Process() = Process.GetProcessesByName("Chrome")
+                Dim index As Integer = 0
+                Do While True
+                    If (index >= processesByName.Length) Then
+                        Exit Do
+                    End If
+                    Dim process As Process = processesByName(index)
+                    Form1.SetProcessWorkingSetSize(process.Handle, -1, -1)
+                    index += 1
+                Loop
+            End If
+        Catch exception1 As Exception
+            Dim ex As Exception = exception1
+            ProjectData.SetProjectError(ex)
+            Dim exception As Exception = ex
+            ProjectData.ClearProjectError()
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+        MsgBox("Reduce RAM is Actived.",, "Active")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+        MsgBox("Restoring data's content. ",, "Deactive")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
---
+++ Memory Cleaning Chrome/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Memory Cleaning Audition")>
+<Assembly: AssemblyDescription("Memory Cleaning Audition")>
+<Assembly: AssemblyCompany("Memory Cleaning Audition")>
+<Assembly: AssemblyProduct("RAM Cleaner # Project")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("Memory Cleaning Audition")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6c620c9c-2dcc-4215-91e1-cfaf9d2a143a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
